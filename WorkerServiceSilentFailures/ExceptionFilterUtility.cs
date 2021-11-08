48167fe49e2cafc2720cdef76079a41107caf925
Alex Machado <alex.machado@invillia.com>
2021-11-08 16:33:40 -0300

    Some refactorings

---
+++ WorkerServiceSilentFailures/ExceptionFilterUtility.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace WorkerServiceSilentFailures
+{
+    /// <summary>
+    /// Essentially returning false from the exception filter prevents the stack from being unwound.
+    /// Catching and rethrowing (even with throw; and not throw exception;) will unwind the stack to this point and then throw the exception again.
+    /// Ref.: https://blog.stephencleary.com/2020/06/a-new-pattern-for-exception-logging.html
+    /// </summary>
+    public static class ExceptionFilterUtility
+    {
+        /// <summary>
+        /// Another scenario is if the catch block actually handles the exception.
+        /// Say, if we know there is an exception that is safe to ignore.
+        /// In that case, use the True helper method so that the exception matches the catch block and the stack is unwound and the exception is handled there.
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public static bool True(Action action)
+        {
+            action();
+            return true;
+        }
+
+        /// <summary>
+        /// If the body of your catch is nothing more than throw.
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        public static bool False(Action action)
+        {
+            action();
+            return false;
+        }
+    }
+}
--- WorkerServiceSilentFailures/Worker.cs
+++ WorkerServiceSilentFailures/Worker.cs
@@ -1,8 +1,9 @@
-using Microsoft.Extensions.Hosting;
+ï»¿using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using static WorkerServiceSilentFailures.ExceptionFilterUtility;
 
 namespace WorkerServiceSilentFailures
 {
@@ -20,37 +21,31 @@ namespace WorkerServiceSilentFailures
         /// <summary>
         /// This problem will surface as BackgroundService instances just stopping, without any indication of a problem. 
         /// What actually happens if ExecuteAsync throws an exception is that the exception is captured and placed on the Task that was returned from ExecuteAsync. 
-        /// The problem is that BackgroundService doesn’t observe that task, so there’s no logging and no process crash - the BackgroundService has completed executing but it just sits there doing nothing. 
+        /// The problem is that BackgroundService doesnâ€™t observe that task, so thereâ€™s no logging and no process crash - the BackgroundService has completed executing but it just sits there doing nothing. 
         /// </summary>
+        /// <inheritdoc cref="https://blog.stephencleary.com/2020/05/backgroundservice-gotcha-silent-failure.html"/>
         /// <param name="stoppingToken"></param>
         /// <returns></returns>
         /// <exception cref="Exception"></exception>
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken) => await Task.Run(async () =>
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            try
+            while (!stoppingToken.IsCancellationRequested)
             {
-                while (!stoppingToken.IsCancellationRequested)
+                try
                 {
                     _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
                     await Task.Delay(1000, stoppingToken);
                 }
+                catch (Exception exc) when (False(() => _logger.LogCritical(exc, "Fatal error")))
+                {
+                    throw;
+                }
+                finally
+                {
+                    _hostApplicationLifetime.StopApplication();
+                }
             }
-            catch (Exception exc) when (False(() => _logger.LogCritical(exc, "Fatal error")))
-            {
-                throw;
-            }
-            finally
-            {
-                _hostApplicationLifetime.StopApplication();
-            }
-        }, stoppingToken);
 
-        /// <summary>
-        /// Essentially returning false from the exception filter prevents the stack from being unwound.
-        /// Catching and rethrowing (even with throw; and not throw exception;) will unwind the stack to this point and then throw the exception again.
-        /// </summary>
-        /// <param name="action"></param>
-        /// <returns></returns>
-        private static bool False(Action action) { action(); return false; }
+        }
     }
 }
